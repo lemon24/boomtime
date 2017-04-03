055d947bb56541ba110327847cc2b42a94d69d2f
Adrian Damian <damian.adrian24@gmail.com>
2017-04-03 23:52:29 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('boomtime/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='boomtime',
+    version=version,
+    author='lemon24',
+    url='https://github.com/lemon24/boomtime',
+    packages=['boomtime'],
+    description="",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
